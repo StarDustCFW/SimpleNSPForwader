19f4edc6f85586e3cc7a855750064a18a97e8ade
Kronos2308 <36446521+Kronos2308@users.noreply.github.com>
2021-05-01 03:18:03 +0100

    initial

---
+++ compile.bat
@@ -0,0 +1,25 @@
+@echo off
+rem Edit This
+set APP_ICON=Icon.jpg
+set APP_TITLE=RetroArch
+set APP_AUTHOR=Kronos2308
+set APP_VERSION=1.9.5
+set APP_TITLEID=05B80C7D3B860000
+set APP_USEL=sdmc:\/switch\/retroarch_switch\/retroarch_switch.nro
+set APP_USE=sdmc:\/switch\/retroarch_switch.nro
+rem The "\" after "/" is required
+make
+if %errorlevel% equ 0 color 0a
+
+make clean
+pause
+set a=%errorlevel%
+echo ------------------------------------------
+
+
+
+echo -----------------------------------
+
+%systemroot%\system32\timeout.exe 20
+%systemroot%\system32\timeout.exe 20
+
