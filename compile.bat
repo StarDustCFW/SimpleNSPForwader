8dcfe9294c5ec7ad0230cf1af4f8da4dfbf5143d
Kronos2308 <36446521+Kronos2308@users.noreply.github.com>
2021-05-23 16:34:48 +0100

    update

--- compile.bat
+++ compile.bat
@@ -5,8 +5,7 @@ set APP_ICON=Icon.jpg
 set APP_TITLE=RetroArch
 set APP_AUTHOR=Kronos2308
 set APP_TITLEID=05B80C7D3B860000
-set APP_USEL=/switch/retroarch_switch.nro
-rem set APP_USE=/switch/retroarch_switch.nro
+set APP_USE=/switch/retroarch_switch.nro
 echo -----------------------------------
 
 rem 
@@ -16,7 +15,7 @@ rem make -C Buildexefs
 rem copy "BuildTools\Buildexefs\build\exefs\main" "BuildTools\exefs\main"
 
 mkdir BuildTools\control
-echo|set /p="sdmc:%APP_USEL%"> BuildTools\romfs\nextArgv
+echo|set /p="sdmc:%APP_USE%"> BuildTools\romfs\nextArgv
 echo|set /p="sdmc:%APP_USE%"> BuildTools\romfs\nextNroPath
 copy %APP_ICON% BuildTools\control\icon_AmericanEnglish.dat
 BuildTools\hacbrewpack.exe --titlepublisher %APP_AUTHOR% --titlename %APP_TITLE% --titleid %APP_TITLEID% --nologo --romfs BuildTools\romfs --controldir BuildTools\control --htmldocdir BuildTools\HtmlDoc --exefsdir BuildTools\exefs -k BuildTools\keys.dat --nspdir .\
